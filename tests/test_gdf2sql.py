f25e6d4f0f408e9eb259e8f6d4e299ed4db3856e
Lucas Bruand <l.bruand.pro@gmail.com>
2024-02-25 21:57:39 +0100

    run stuff super fast on sqlite

--- tests/test_gdf2sql.py
+++ tests/test_gdf2sql.py
@@ -3,6 +3,8 @@ from typing import List
 import geopandas as gpd
 import pandas as pd
 import psycopg2
+import sqlglot
+from sqlalchemy import create_engine, text
 
 from gdf2sql.gdf2sql import build_vtable, VTable, build_test_sql_query
 
@@ -71,3 +73,24 @@ def test_inject_queries(postgis_server):
             ix, name, geom = city
             assert name == 'Brasilia'
 
+
+def test_inject_queries_postgres_to_sqlite():
+
+    inner_query = "WITH A(v) as (values (0), (1)) SELECT A.v, name " \
+                  " FROM nyc_subway_stats, A " \
+                  "WHERE nyc_subway_stats.name = 'Brasilia'"
+    df = generate_example_df()
+    tables: List[VTable] = [(build_vtable("nyc_subway_stats", df))]
+    inner_query_as_sqlite = sqlglot.transpile(inner_query, read="postgres", write="sqlite")[0]
+    result_query = build_test_sql_query(tables, inner_query_as_sqlite)
+
+    assert result_query is not None
+
+    mocked_db = create_engine('sqlite://')
+    with mocked_db.connect() as connection:
+        result = connection.execute(text(result_query))
+        mydata = result.fetchall()
+    assert len(mydata) == 2
+    city, *_ = mydata
+    ix, name = city
+    assert name == 'Brasilia'
